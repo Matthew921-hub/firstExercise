e61e308206e58799679512560e70509481773eb5
Matthew <vaskovitch.matvej@yandex.ru>
2021-08-23 22:13:54 +0300

    First exercise Udemy

---
+++ js/script.js
@@ -0,0 +1,23 @@
+"use strict";
+
+const numberOfFilms = +prompt("Number of films you watch:", "");
+
+let personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false    
+};
+
+let nameMov = prompt("One of the last seen movies?", ""),
+    starMov = prompt("How can you describe it?", "");
+
+personalMovieDB.movies[nameMov] = starMov;
+
+nameMov = prompt("One of the last seen movies?", ""),
+starMov = prompt("How can you describe it?", "");
+
+personalMovieDB.movies[nameMov] = starMov;
+
+console.log(personalMovieDB);
