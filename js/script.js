a325f87f9fa8c4bb2b2fce07ba484de8ce53326f
Matthew <vaskovitch.matvej@yandex.ru>
2021-08-23 23:59:01 +0300

    Second exercise Udemy.

--- js/script.js
+++ js/script.js
@@ -10,14 +10,26 @@ const personalMovieDB = {
     privat: false    
 };
 
-let nameMov = prompt("One of the last seen movies?", ""),
+if(personalMovieDB.count < 10){
+    console.log('You watch not to many movies.');
+} else if(personalMovieDB.count >= 10 && personalMovieDB.count <= 30){
+    console.log('You are classic viewer.');
+} else if(personalMovieDB.count > 30){
+    console.log("You are movieman!");
+} else {
+    console.log("Error!");
+}
+
+for(let i = 0; i < 2; i++){
+    let nameMov = prompt("One of the last seen movies?", ""),
     starMov = prompt("How can you describe it?", "");
-
-personalMovieDB.movies[nameMov] = starMov;
-
-nameMov = prompt("One of the last seen movies?", ""),
-starMov = prompt("How can you describe it?", "");
-
-personalMovieDB.movies[nameMov] = starMov;
+    if( nameMov != null && starMov != null && nameMov != "" && starMov != "" && nameMov < 50 && starMov < 50){
+        personalMovieDB.movies[nameMov] = starMov;
+        console.log("Done");
+    } else {
+        i--;
+        console.log("Error");
+    }
+}
 
 console.log(personalMovieDB);
